8d869c6fa675f6fb2629a915c7c75cfd49581f08
DESKTOP-1T8SV8I\alisa <danalisa18@stud.ase.ro>
2021-03-04 08:23:27 +0200

    Seminar2 second commit

--- Seminar2/src/ro/ase/cts/clase/Program.java
+++ Seminar2/src/ro/ase/cts/clase/Program.java
@@ -1,5 +1,7 @@
 package ro.ase.cts.clase;
 
+import ro.ase.cts.readere.AngajatReader;
+
 import java.io.FileNotFoundException;
 import java.util.List;
 
@@ -8,7 +10,7 @@ public class Program {
 	public static void main(String[] args) {
 		List<Angajat> listaAngajati;
 		try {
-			listaAngajati = Utils.readAngajati("angajati.txt");
+			listaAngajati = AngajatReader.readAngajati("angajati.txt");
 			for(Angajat angajat:listaAngajati)
 				System.out.println(angajat.toString());
 		} catch (FileNotFoundException e) {
