34d645a27213c3d050129c4f1c9783de5b45c561
DESKTOP-1T8SV8I\alisa <danalisa18@stud.ase.ro>
2021-03-11 08:32:22 +0200

    Seminar 3 second commit

--- Seminar2/src/ro/ase/cts/clase/Program.java
+++ Seminar2/src/ro/ase/cts/clase/Program.java
@@ -1,6 +1,5 @@
 package ro.ase.cts.clase;
 
-import ro.ase.cts.readere.AngajatReader;
 import ro.ase.cts.readere.AplicantReader;
 import ro.ase.cts.readere.StudentReader;
 
@@ -9,20 +8,21 @@ import java.util.List;
 
 public class Program {
 
-	public static List<Aplicant> citesteAplicanti(AplicantReader aplicantReader)throws NumberFormatException,FileNotFoundException{
-		return aplicantReader.citesteAplicanti();
-	}
-	public static void main(String[] args) {
-		List<Aplicant> listaAplicanti;
-		AplicantReader aplicantReader=new StudentReader("studenti.txt");
-		try {
-			listaAplicanti = citesteAplicanti(aplicantReader);
-			for(Aplicant angajat:listaAplicanti)
-				System.out.println(angajat.toString());
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+    public static List<Aplicant> citesteAplicanti(AplicantReader aplicantReader) throws NumberFormatException, FileNotFoundException {
+        return aplicantReader.citesteAplicanti();
+    }
+
+    public static void main(String[] args) {
+        List<Aplicant> listaAplicanti;
+        AplicantReader aplicantReader = new StudentReader("studenti.txt");
+        try {
+            listaAplicanti = citesteAplicanti(aplicantReader);
+            for (Aplicant angajat : listaAplicanti)
+                System.out.println(angajat.toString());
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
 }
